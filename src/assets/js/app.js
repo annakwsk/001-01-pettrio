27262d64ea94af28d3ab563454fb7d68590cbacb
Anna Kawasaki <yuzuuriruru@gmail.com>
2025-11-17 18:55:53 +0900

    add

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,7 @@
 import Common from "./_common.js";
-import Slider from "./_slider.js";
-import Form from "./_form.js";
-import Q from "./_q.js";
 import Menu from "./_menu.js";
-import Ending from "./_ending.js";
 
 $(function () {
-  Slider();
-  Form();
   Common();
-  Q();
   Menu();
-  Ending();
 });
